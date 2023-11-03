48cd11eb1e187b868799c6c24e1a883ffad833d5
tonytwei <tony.t.wei@gmail.com>
2023-11-03 19:00:33 +1100

    added env variables, mongodb query by questionID

--- server/server.js
+++ server/server.js
@@ -4,23 +4,25 @@ const mongoose = require('mongoose');
 const Blog = require('../client/models/blog');
 const Question = require('../client/models/question');
 const app = express();
+require('dotenv').config();
 
 // connect to mongodb
-const port = 3005;
-const dbURI = 'REPLACE'; // modified dbURI
+const port = process.env.PORT || 3000;
+const dbURI = process.env.DB_URI;
 mongoose.connect(dbURI)
 	.then((result) => app.listen(port))
 	.catch((err) => console.log(err));
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client/pages'));
-
 app.use(express.static(path.join(__dirname, '../client/public')));
 
-// question routes
-// TODO: remove later, temp 
+
+
+// mongodb question routes, still being worked on
 app.get('/add-question', (req, res) => {
-	const data = require(`../client/public/assets/questions/0242.json`);
+	const data = require(`../client/public/assets/questions/0217.json`);
 	const question = new Question(data);
 
 	question.save()
@@ -45,18 +47,46 @@ app.get('/all-questions', (req, res) => {
 app.get('/quiz/:id',  (req, res) => {
 	const id = req.params.id;
 	console.log(id);
-	Question.findById(id)
+	Question.findOne({ id: id })
 		.then((result) => {
-			const data = result;
-			res.render('quiz', { data })
+			if (result) {
+				const data = result;
+				res.render('quiz', { data })
+			} else {
+				res.status(404).send('Question not found');
+			}
 		})
 		.catch((err) => {
 			console.log(err);
+			res.status(500).send('Internal Server Error');
 		});
 })
 
+const difficulties = ["Easy", "Medium"];
+const topics = ["Arrays"];
+Question.find({
+	difficulty: { $in: difficulties },
+	topics: { $in: topics}
+})
+	.select('id title difficulty')
+	.exec()
+	.then((result) => {
+		console.log(result);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
-// original routes
+
+
+
+
+
+
+
+
+
+// oringinal ejs routes
 app.get('/', (req, res) => {
 	res.render('index');
 });
